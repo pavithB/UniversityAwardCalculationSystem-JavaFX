815fd93e7adb1dd7314abd12db321a74009025cb
pavithB <pavithbuddhima@gmail.com>
2017-03-06 12:41:59 +0530

    University Award Calculation System

---
+++ UOG2/src/application/DataBase.java
@@ -0,0 +1,140 @@
+package application;
+
+public class DataBase {
+
+	private static int module51;
+	private static int module52;
+	private static int module53;
+	private static int module54;
+	private static int module55;
+	private static int module56;
+
+	private static int module61;
+	private static int module62;
+	private static int module63;
+	private static int module64;
+	private static int module65;
+
+	private static String studentID;
+	private static String studentName;
+
+
+	public static String getStudentName() {
+		return studentName;
+	}
+
+	public static void setStudentName(String studentName) {
+		DataBase.studentName = studentName;
+	}
+
+	private static int qualify; 
+
+	public static int getQualify() {
+		return qualify;
+	}
+
+	public static void setQualify(int qualify) {
+		DataBase.qualify = qualify;
+	}
+
+	
+
+	public static int getModule51() {
+		return module51;
+	}
+
+	public static void setModule51(int module51) {
+		DataBase.module51 = module51;
+	}
+
+	public static int getModule52() {
+		return module52;
+	}
+
+	public static void setModule52(int module52) {
+		DataBase.module52 = module52;
+	}
+
+	public static int getModule53() {
+		return module53;
+	}
+
+	public static void setModule53(int module53) {
+		DataBase.module53 = module53;
+	}
+
+	public static int getModule54() {
+		return module54;
+	}
+
+	public static void setModule54(int module54) {
+		DataBase.module54 = module54;
+	}
+
+	public static int getModule55() {
+		return module55;
+	}
+
+	public static void setModule55(int module55) {
+		DataBase.module55 = module55;
+	}
+
+	public static int getModule56() {
+		return module56;
+	}
+
+	public static void setModule56(int module56) {
+		DataBase.module56 = module56;
+	}
+
+	public static int getModule61() {
+		return module61;
+	}
+
+	public static void setModule61(int module61) {
+		DataBase.module61 = module61;
+	}
+
+	public static int getModule62() {
+		return module62;
+	}
+
+	public static void setModule62(int module62) {
+		DataBase.module62 = module62;
+	}
+
+	public static int getModule63() {
+		return module63;
+	}
+
+	public static void setModule63(int module63) {
+		DataBase.module63 = module63;
+	}
+
+	public static int getModule64() {
+		return module64;
+	}
+
+	public static void setModule64(int module64) {
+		DataBase.module64 = module64;
+	}
+
+	public static int getModule65() {
+		return module65;
+	}
+
+	public static void setModule65(int module65) {
+		DataBase.module65 = module65;
+	}
+
+	public static String getStudentID() {
+		return studentID;
+	}
+
+	public static void setStudentID(String studentID) {
+		DataBase.studentID = studentID;
+	}
+
+
+	}
+
